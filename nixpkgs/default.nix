2e4602b63f058842230f0ab7693cca0af98facca
David Meister <thedavidmeister@gmail.com>
2019-07-09 01:26:09 +1000

    lint

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,6 +1,7 @@
 let
+  params = {  };
   # https://vaibhavsagar.com/blog/2018/05/27/quick-easy-nixpkgs-pinning/
-  inherit (import <nixpkgs> {}) fetchgit;
+  inherit (import <nixpkgs> params) fetchgit;
 
   rev = "e9a5a28aa657bdc3170ed378693f18dfbb79fdb5";
   sha256 = "08ajj01v6jqhy9qhw5r4rp18ws77xfphdpzbsbb8461hqq48vz4p";
@@ -10,7 +11,7 @@ let
    url = "https://github.com/NixOS/nixpkgs.git";
    rev = rev;
    sha256 = sha256;
-  }) { };
+  }) params;
 
 in
 pkgs // { rev = rev; }
