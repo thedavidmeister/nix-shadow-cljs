7c416d374cb2a2412361fcb86f0eb3d9e33b3a4b
David Meister <thedavidmeister@gmail.com>
2019-07-08 21:48:28 +1000

    init with shadow-cljs.edn

--- init/default.nix
+++ init/default.nix
@@ -3,12 +3,28 @@ let
 
   name = "nsc-init";
 
+  shadow-cljs-edn = ''
+;; shadow-cljs configuration
+{
+ :deps true
+ :builds
+ {
+  :index
+  {:target :browser
+   :output-dir "public/js/index"
+   :asset-path "/public/js/index"
+   :modules
+   {:main
+    {:entries [index.mount]}}}}}
+'';
+
   # simple npx wrapper to avoid global install
   script = pkgs.writeShellScriptBin name ''
   set -euxo pipefail
-  npm init
+  npm init -y
   npm install --save shadow-cljs@2.8.40
   npm install --save local-web-server@3.0.4
+  echo '${shadow-cljs-edn}' > ./shadow-cljs.edn
   '';
 
 in
