814b0f71bab0008a83cfbe24948001b9c6374d6a
David Meister <thedavidmeister@gmail.com>
2019-09-14 15:33:43 -0400

    bump nixpkgs

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -3,8 +3,8 @@ let
   # https://vaibhavsagar.com/blog/2018/05/27/quick-easy-nixpkgs-pinning/
   inherit (import <nixpkgs> params) fetchFromGitHub;
 
-  rev = "e9a5a28aa657bdc3170ed378693f18dfbb79fdb5";
-  sha256 = "08ajj01v6jqhy9qhw5r4rp18ws77xfphdpzbsbb8461hqq48vz4p";
+  rev = "8a530a0bab1f1d28723e690f702f7a103dc6741d";
+  sha256 = "16b76msp6xhhfi7hiq8xh0z2ir5lwys5rqy85l5vg81vp6xkgyc3";
 
   # nixos holo-host channel @ 2019-04-02
   pkgs = import (fetchFromGitHub {
