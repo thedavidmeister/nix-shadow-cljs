242fc7c8674b7e01da9c46848cb81d9a9d9bbcb0
David Meister <thedavidmeister@gmail.com>
2019-09-14 13:42:09 -0400

    fetchFromGitHub

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,14 +1,15 @@
 let
   params = {  };
   # https://vaibhavsagar.com/blog/2018/05/27/quick-easy-nixpkgs-pinning/
-  inherit (import <nixpkgs> params) fetchgit;
+  inherit (import <nixpkgs> params) fetchFromGitHub;
 
   rev = "e9a5a28aa657bdc3170ed378693f18dfbb79fdb5";
   sha256 = "08ajj01v6jqhy9qhw5r4rp18ws77xfphdpzbsbb8461hqq48vz4p";
 
   # nixos holo-host channel @ 2019-04-02
-  pkgs = import (fetchgit {
-   url = "https://github.com/NixOS/nixpkgs.git";
+  pkgs = import (fetchFromGitHub {
+   owner = "NixOS";
+   repo = "nixpkgs";
    rev = rev;
    sha256 = sha256;
   }) params;
